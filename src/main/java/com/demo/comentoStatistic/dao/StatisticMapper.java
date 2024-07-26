ba08d608579efd7f2f5243586a0d5d054f06f61b
wobrorry <128110758+wobrorry@users.noreply.github.com>
2024-07-26 23:36:02 +0900

    3rd assignment

---
+++ src/main/java/com/demo/comentoStatistic/dao/StatisticMapper.java
@@ -0,0 +1,13 @@
+package com.demo.comentoStatistic.dao;
+
+import com.demo.comentoStatistic.dto.YearCountDto;
+import com.demo.comentoStatistic.dto.YearMonthCountDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StatisticMapper {
+
+    YearCountDto selectYearLogin(String year);
+    YearMonthCountDto selectYearMonthLogin(String yearMonth);
+
+}
---
+++ src/test/java/com/demo/comentoStatistic/StatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.comentoStatistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
